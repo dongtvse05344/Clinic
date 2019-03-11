99b970894e4846fb71399824fccec25865eb5fce
thuongdth <44562594+thuongdth@users.noreply.github.com>
2019-03-11 15:19:16 +0700

    Info(Get Clear )Prescription

--- Clinic/Prescription.Designer.cs
+++ Clinic/Prescription.Designer.cs
@@ -30,21 +30,22 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.rbFemale = new System.Windows.Forms.RadioButton();
-            this.rbMale = new System.Windows.Forms.RadioButton();
-            this.btnGetByPhone = new System.Windows.Forms.Button();
-            this.btnOk = new System.Windows.Forms.Button();
-            this.txtId = new System.Windows.Forms.TextBox();
-            this.txtName = new System.Windows.Forms.TextBox();
-            this.txtAddress = new System.Windows.Forms.TextBox();
-            this.txtPhone = new System.Windows.Forms.TextBox();
             this.txtBirthDate = new System.Windows.Forms.DateTimePicker();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnGetByPhone = new System.Windows.Forms.Button();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRefresh = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -60,6 +61,7 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.btnRefresh);
             this.panel1.Controls.Add(this.txtBirthDate);
             this.panel1.Controls.Add(this.txtPhone);
             this.panel1.Controls.Add(this.txtAddress);
@@ -80,70 +82,61 @@
             this.panel1.Size = new System.Drawing.Size(280, 248);
             this.panel1.TabIndex = 1;
             // 
-            // label2
+            // txtBirthDate
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 21);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(22, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Id :";
+            this.txtBirthDate.Location = new System.Drawing.Point(60, 138);
+            this.txtBirthDate.Name = "txtBirthDate";
+            this.txtBirthDate.Size = new System.Drawing.Size(134, 20);
+            this.txtBirthDate.TabIndex = 14;
             // 
-            // label3
+            // txtPhone
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 51);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 13);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Name :";
+            this.txtPhone.Location = new System.Drawing.Point(60, 112);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(134, 20);
+            this.txtPhone.TabIndex = 13;
             // 
-            // label4
+            // txtAddress
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(3, 82);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 13);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Address :";
+            this.txtAddress.Location = new System.Drawing.Point(60, 82);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(134, 20);
+            this.txtAddress.TabIndex = 12;
             // 
-            // label5
+            // txtName
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(3, 115);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(44, 13);
-            this.label5.TabIndex = 3;
-            this.label5.Text = "Phone :";
+            this.txtName.Location = new System.Drawing.Point(60, 51);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(134, 20);
+            this.txtName.TabIndex = 11;
             // 
-            // label6
+            // txtId
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(3, 146);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(60, 13);
-            this.label6.TabIndex = 4;
-            this.label6.Text = "Birth Date :";
+            this.txtId.Location = new System.Drawing.Point(60, 18);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(134, 20);
+            this.txtId.TabIndex = 10;
             // 
-            // label7
+            // btnOk
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(3, 178);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(48, 13);
-            this.label7.TabIndex = 5;
-            this.label7.Text = "Gender :";
+            this.btnOk.Location = new System.Drawing.Point(140, 211);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 9;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
             // 
-            // rbFemale
+            // btnGetByPhone
             // 
-            this.rbFemale.AutoSize = true;
-            this.rbFemale.Location = new System.Drawing.Point(60, 176);
-            this.rbFemale.Name = "rbFemale";
-            this.rbFemale.Size = new System.Drawing.Size(59, 17);
-            this.rbFemale.TabIndex = 6;
-            this.rbFemale.TabStop = true;
-            this.rbFemale.Text = "Female";
-            this.rbFemale.UseVisualStyleBackColor = true;
+            this.btnGetByPhone.Location = new System.Drawing.Point(200, 110);
+            this.btnGetByPhone.Name = "btnGetByPhone";
+            this.btnGetByPhone.Size = new System.Drawing.Size(75, 23);
+            this.btnGetByPhone.TabIndex = 8;
+            this.btnGetByPhone.Text = "Get";
+            this.btnGetByPhone.UseVisualStyleBackColor = true;
+            this.btnGetByPhone.Click += new System.EventHandler(this.btnGetByPhone_Click);
             // 
             // rbMale
             // 
@@ -156,61 +149,80 @@
             this.rbMale.Text = "Male";
             this.rbMale.UseVisualStyleBackColor = true;
             // 
-            // btnGetByPhone
+            // rbFemale
             // 
-            this.btnGetByPhone.Location = new System.Drawing.Point(200, 110);
-            this.btnGetByPhone.Name = "btnGetByPhone";
-            this.btnGetByPhone.Size = new System.Drawing.Size(75, 23);
-            this.btnGetByPhone.TabIndex = 8;
-            this.btnGetByPhone.Text = "Get";
-            this.btnGetByPhone.UseVisualStyleBackColor = true;
-            this.btnGetByPhone.Click += new System.EventHandler(this.btnGetByPhone_Click);
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Location = new System.Drawing.Point(60, 176);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(59, 17);
+            this.rbFemale.TabIndex = 6;
+            this.rbFemale.TabStop = true;
+            this.rbFemale.Text = "Female";
+            this.rbFemale.UseVisualStyleBackColor = true;
             // 
-            // btnOk
+            // label7
             // 
-            this.btnOk.Location = new System.Drawing.Point(97, 208);
-            this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(75, 23);
-            this.btnOk.TabIndex = 9;
-            this.btnOk.Text = "OK";
-            this.btnOk.UseVisualStyleBackColor = true;
-            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 178);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Gender :";
             // 
-            // txtId
+            // label6
             // 
-            this.txtId.Location = new System.Drawing.Point(60, 18);
-            this.txtId.Name = "txtId";
-            this.txtId.ReadOnly = true;
-            this.txtId.Size = new System.Drawing.Size(134, 20);
-            this.txtId.TabIndex = 10;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Birth Date :";
             // 
-            // txtName
+            // label5
             // 
-            this.txtName.Location = new System.Drawing.Point(60, 51);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(134, 20);
-            this.txtName.TabIndex = 11;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Phone :";
             // 
-            // txtAddress
+            // label4
             // 
-            this.txtAddress.Location = new System.Drawing.Point(60, 82);
-            this.txtAddress.Name = "txtAddress";
-            this.txtAddress.Size = new System.Drawing.Size(134, 20);
-            this.txtAddress.TabIndex = 12;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Address :";
             // 
-            // txtPhone
+            // label3
             // 
-            this.txtPhone.Location = new System.Drawing.Point(60, 112);
-            this.txtPhone.Name = "txtPhone";
-            this.txtPhone.Size = new System.Drawing.Size(134, 20);
-            this.txtPhone.TabIndex = 13;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Name :";
             // 
-            // txtBirthDate
+            // label2
             // 
-            this.txtBirthDate.Location = new System.Drawing.Point(60, 138);
-            this.txtBirthDate.Name = "txtBirthDate";
-            this.txtBirthDate.Size = new System.Drawing.Size(134, 20);
-            this.txtBirthDate.TabIndex = 14;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Id :";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(44, 211);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 15;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
             // 
             // Prescription
             // 
@@ -248,5 +260,6 @@
         private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.TextBox txtId;
         private System.Windows.Forms.DateTimePicker txtBirthDate;
+        private System.Windows.Forms.Button btnRefresh;
     }
 }
